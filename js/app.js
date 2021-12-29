b639b0afa8a7159445eded8124fc74c0b93150ca
sadra rahbar <sadrarahbar@gmail.com>
2021-12-29 16:36:01 +0330

    Build Sidebar

---
+++ js/app.js
@@ -0,0 +1,7 @@
+function toggler_click() {
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('nav--expanded');
+    const social = document.getElementById('social');
+    social.classList.toggle('social--expanded');
+}
+
